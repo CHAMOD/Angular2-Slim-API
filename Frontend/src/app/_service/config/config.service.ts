77638b5be9b47f66acdac8908cd022c0a771008d
abc <abc@example.com>
2018-04-27 14:53:02 +0530

    API path error fixed

--- Frontend/src/app/_service/config/config.service.ts
+++ Frontend/src/app/_service/config/config.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ConfigService {
 
-  apiPath='http://localhost/api_repairshop/';
+  apiPath='http://localhost/Frontend/';
 
   constructor() { }
 
